c5e949a943dbae962ef92e33689761fc33ee876d
Angel L. P <59593766+angelphanth@users.noreply.github.com>
2025-09-04 15:47:55 +0200

    add tests from demo

--- src/python_package/mockup.py
+++ src/python_package/mockup.py
@@ -13,5 +13,33 @@ def hello_world(n: int) -> str:
     -------
     str
         str of 'hello world' n-times
+
+    Examples
+    --------
+    >>> hello_world(3)
+    'hello world hello world hello world'
     """
     return " ".join(repeat("hello world", n))
+
+
+def saved_world(filename: str) -> int:
+    """
+    Count how many times 'hello world' is in a file.
+
+    Parameters
+    ----------
+    filename : str
+        The file to read
+
+    Returns
+    -------
+    int
+        How many times 'hello world' is in the file
+
+    Examples
+    --------
+    >>> saved_world("not-real.txt") # doctest: +SKIP
+    """
+    with open(filename, "r") as f:
+        content = f.read()
+    return content.count("hello world")
--- tests/test_mockup.py
+++ tests/test_mockup.py
@@ -1,7 +1,29 @@
-from python_package import hello_world
-
+from python_package import hello_world, saved_world
+import pytest
 
 def test_hello_world_3times():
     expected = "hello world hello world hello world"
     result = hello_world(3)
     assert result == expected
+
+
+@pytest.mark.xfail(raises=TypeError)
+def test_hello_world_str():
+    hello_world("3")
+
+
+@pytest.fixture
+def temp_file():
+    # set up
+    filename = "temp_hellooo.txt"
+    with open(filename, "w") as f:
+        f.write("hello world hello world hello world")
+    yield filename
+    # clean up
+    import os
+    os.remove(filename)
+
+
+def test_saved_world_3times(temp_file):
+    result = saved_world(temp_file)
+    assert result == 3
